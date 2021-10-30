2dda55d6a1a2b09082ac489ce52a15e5fe17e9d0
Fernando-E-Jimenez <ferve91@gmail.com>
2021-10-30 10:44:08 -0300

    Cambiando al verdadero repo

---
+++ HO 00/JSI.text.js
@@ -0,0 +1,25 @@
+// En estas primeras 6 preguntas, reemplaza 'null' por la respuesta correcta
+
+// Crea una variable "String", puede contener lo que quieras:
+
+const nuevaString = null;
+
+// Crea una variable numerica, puede ser cualquier numero:
+
+const nuevoNum = null;
+
+// Crea una variable booleana:
+
+const nuevaBool = null;
+
+// Resuelve el siguiente problema matematico:
+
+const nuevaResta = 10 - null === 5;
+
+// Resuelve el siguiente problema matematico:
+
+const nuevaMultiplicacion = 10 * null === 40;
+
+// Resuelve el siguiente problema matematico:
+
+const nuevoModulo = 21 % 5 === null;
